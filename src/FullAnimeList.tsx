dee1d235950c2a0867ed57fd044b2d4c9e5f9d60
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-02-28 13:24:10 +0700

    extract the fetch anime logic to different fn
    the fetch anime logic can be found is seperated component, to hinder the repeated typing and can cause different error in different component
    
    and now, we utilize useMemo hook as #6 issue addressed

--- src/FullAnimeList.tsx
+++ src/FullAnimeList.tsx
@@ -1,40 +1,14 @@
-import { useEffect, useState } from 'react';
 import config from './config';
-import { AnimeList, animeType } from './components/AnimeList';
+import { AnimeList } from './components/AnimeList';
+import fetchAnimes from './fetchAnimes';
 
 export default function FullAnimeList() {
-  const [animes, setAnimes] = useState<animeType[]>([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState<string>();
-
-  useEffect(() => {
-    setIsFetching(true);
-    const abortController = new AbortController();
-    fetch(config.BASE_URL, { signal: abortController.signal })
-      .then((responses) => {
-        if (!responses.ok) {
-          throw new Error(
-            "There's an error when fetching data, please try again...",
-          );
-        }
-        return responses.json();
-      })
-      .then((anime) => {
-        setAnimes(anime.data);
-      })
-      .catch((error) => {
-        setError(error.message);
-      })
-      .finally(() => setIsFetching(false));
-    return () => {
-      abortController.abort();
-    };
-  }, []);
+  const { animes, isFetching, error } = fetchAnimes(config.BASE_URL);
 
   return (
     <>
       {isFetching && 'Loading...'}
-      {error && <p>{error}</p>}
+      {error && <p>{error.message}</p>}
       {animes && <AnimeList animes={animes} />}
     </>
   );
