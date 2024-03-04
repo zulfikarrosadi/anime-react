0c7f3d720d607c4a2dce89a0200c66748290aeaf
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-03-04 23:08:56 +0700

    show searchBar componenet in main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Outlet, Link } from 'react-router-dom';
+import SearchBar from './components/SearchBar';
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
           </li>
         </ul>
       </nav>
+      <SearchBar />
       <Outlet />
     </>
   );
